a7a09d58d3ff8c0578f4812d9364e837c2c5c9d4
Alireza Askarian <ar.asgarian1389@gmail.com>
2023-09-22 20:42:20 +0330

    startProject3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("mm");
         System.out.println("Hello world!");
+        System.out.printf("maw maw maw");
     }
 }
